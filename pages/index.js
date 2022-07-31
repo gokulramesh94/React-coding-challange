20000d00dbe649f25bf706e8bbfc69483745992f
Gokul <gokulpulari@gmail.com>
2022-07-31 08:54:35 -0400

    Implemented lazy loading on page end.

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,48 @@
 import { useEffect, useState } from 'react';
+import _ from 'lodash';
 import Head from 'next/head';
+import Image from '../components/Image/Image';
 import List from '../components/List/List';
 import ImageService from '../services/image.service';
 
 export default function Home() {
   const [imageList, setImageList] = useState([]);
   const [pageNo, setPageNo] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const throttledScroll = _.throttle(handleScroll, 500);
+    fetchImages();
+    window.addEventListener('scroll', throttledScroll);
+    return () => window.removeEventListener('scroll', throttledScroll);
+  }, []);
 
   useEffect(() => {
     fetchImages();
-  }, []);
+  }, [pageNo]);
+
+  const handleScroll = () => {
+    const wrappedElement =
+      document.getElementsByClassName('MuiImageList-root')[0];
+    if (!isBottom(wrappedElement)) return;
+    setPageNo((oldPageNo) => oldPageNo + 1);
+  };
+
+  const isBottom = (element) => {
+    return element.getBoundingClientRect().bottom <= window.innerHeight;
+  };
 
   const fetchImages = () => {
+    setIsLoading(true);
     ImageService.fetchImageList(pageNo)
       .then((response) => {
         setImageList(() => {
           return [...imageList, ...response];
         });
+        setIsLoading(false);
       })
       .catch((error) => {
+        setIsLoading(false);
         console.error('Exception ImageService -> fetchImageList : ', error);
       });
   };
@@ -31,6 +55,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <List data={imageList} viewItem={(image) => handleSelectedImage(image)} />
+      {isLoading ? <div className="loading">Loading more images...</div> : null}
     </div>
   );
 }
