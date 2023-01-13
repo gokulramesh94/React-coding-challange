6fb3efef20f6e01a5040601ec41d1b662c46b7fe
Gokul <gokulpulari@gmail.com>
2023-01-13 11:29:19 -0800

    Updated API endpoint

--- helpers/axios.helper.js
+++ helpers/axios.helper.js
@@ -9,7 +9,7 @@ import { STRING_CONSTANTS } from '../constants/string.constant';
 
 const API_BASE_URL =
   STRING_CONSTANTS?.NEXT_PUBLIC_API_BASE_URL ||
-  'https://react-challenge-photo-grid-api.herokuapp.com/api';
+  'https://0702-2604-3d08-5982-c200-880a-3a58-c00d-20f5.ngrok.io/api';
 
 export const Axios = axios.create({
   baseURL: API_BASE_URL
