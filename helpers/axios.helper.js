4b0c4b3912e7296e83809362f964bb59d5e45d94
Gokul <gokulpulari@gmail.com>
2023-01-16 13:57:55 -0800

    Updated API endpoint

--- helpers/axios.helper.js
+++ helpers/axios.helper.js
@@ -9,7 +9,7 @@ import { STRING_CONSTANTS } from '../constants/string.constant';
 
 const API_BASE_URL =
   STRING_CONSTANTS?.NEXT_PUBLIC_API_BASE_URL ||
-  'https://0702-2604-3d08-5982-c200-880a-3a58-c00d-20f5.ngrok.io/api';
+  'https://photo-grid-api.onrender.com/api';
 
 export const Axios = axios.create({
   baseURL: API_BASE_URL
